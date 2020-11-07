a7e6db1f6df03dd0c975527135cb3af361fe12d2
RoadJDK <58588256+RoadJDK@users.noreply.github.com>
2020-11-07 23:19:27 +0100

    Added Project

---
+++ SpamBot/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SpamBot
+{
+    partial class SpamBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Active = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(152, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SpamBot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(106, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(245, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "press f11 to toggle!";
+            // 
+            // Active
+            // 
+            this.Active.AutoSize = true;
+            this.Active.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Active.ForeColor = System.Drawing.Color.Red;
+            this.Active.Location = new System.Drawing.Point(184, 268);
+            this.Active.Name = "Active";
+            this.Active.Size = new System.Drawing.Size(69, 26);
+            this.Active.TabIndex = 2;
+            this.Active.Text = "active";
+            // 
+            // SpamBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(476, 424);
+            this.Controls.Add(this.Active);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "SpamBot";
+            this.Text = "SpamBot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Active;
+    }
+}
+
---
+++ SpamBot/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace SpamBot
+{
+    public partial class SpamBot : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        const int MYACTION_HOTKEY_ID = 1;
+        private Timer _myTimer;
+
+        bool isActive = false;
+
+        public SpamBot()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_ID, 0, (int)Keys.F11);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _myTimer = new Timer();
+            _myTimer.Interval = 100; // 1 millisecond
+            _myTimer.Tick += new EventHandler(MyTimer_Tick);
+            _myTimer.Start();
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            if(isActive)
+            {
+                StartSpaming();
+            }
+        }
+
+        private void StartSpaming()
+        {
+            var rnd = new Random();
+            var builder = new StringBuilder();
+            for (var i = 0; i < rnd.Next(2, 16);i++)
+            {
+                builder.Append(rnd.Next(9));
+            }
+            SendKeys.Send(builder.ToString());
+            SendKeys.Send("{Enter}");
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_ID)
+            {
+                toggle();
+            }
+            base.WndProc(ref m);
+        }
+
+        private void toggle()
+        {
+            isActive = !isActive;
+
+            //toggle color
+            if (isActive)
+            {
+                Active.ForeColor = Color.Green;
+            }
+            else
+            {
+                Active.ForeColor = Color.Red;
+            }
+        }
+
+    }
+}
